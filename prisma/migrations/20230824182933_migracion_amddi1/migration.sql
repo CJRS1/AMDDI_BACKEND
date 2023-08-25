cd9f97e01608cc4ff2d6822f553ae2493ae02960
CJRS1 <reyes.christian@pucp.pe>
2023-08-24 19:09:24 -0700

    Se añade la migración

---
+++ prisma/migrations/20230824182933_migracion_amddi1/migration.sql
@@ -0,0 +1,121 @@
+-- CreateTable
+CREATE TABLE `usuarios` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `pwd_hash` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `nombre` VARCHAR(50) NOT NULL,
+    `apellido_paterno` VARCHAR(50) NOT NULL,
+    `apellido_materno` VARCHAR(50) NOT NULL,
+    `dni` VARCHAR(8) NOT NULL,
+    `celular` VARCHAR(18) NULL,
+    `departamento` VARCHAR(50) NOT NULL,
+    `carrera` VARCHAR(30) NOT NULL,
+    `rol` VARCHAR(191) NOT NULL DEFAULT 'usuario',
+    `verification_code` VARCHAR(4) NULL,
+    `pdf_url` VARCHAR(255) NULL,
+    `monto_pagado` DOUBLE NULL DEFAULT 0,
+    `monto_total` DOUBLE NULL DEFAULT 0,
+    `created_at` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `usuarios_email_key`(`email`),
+    UNIQUE INDEX `usuarios_dni_key`(`dni`),
+    UNIQUE INDEX `usuarios_celular_key`(`celular`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `asesores` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `pwd_hash` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `nombre` VARCHAR(50) NOT NULL,
+    `apellido_materno` VARCHAR(50) NOT NULL,
+    `apellido_paterno` VARCHAR(50) NOT NULL,
+    `dni` VARCHAR(8) NOT NULL,
+    `rol` VARCHAR(191) NOT NULL DEFAULT 'asesor',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `asesores_email_key`(`email`),
+    UNIQUE INDEX `asesores_dni_key`(`dni`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `especialidades` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre_especialidad` VARCHAR(100) NOT NULL,
+
+    UNIQUE INDEX `especialidades_nombre_especialidad_key`(`nombre_especialidad`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `usuario_servicio` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_usuario` INTEGER NOT NULL,
+    `id_servicio` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `servicios` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombre_servicio` VARCHAR(200) NOT NULL,
+
+    UNIQUE INDEX `servicios_nombre_servicio_key`(`nombre_servicio`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `asignaciones` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_asesor` INTEGER NOT NULL,
+    `id_usuarios` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `administradores` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `pwd_hash` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `nombre` VARCHAR(50) NOT NULL,
+    `apellido_paterno` VARCHAR(50) NOT NULL,
+    `apellido_materno` VARCHAR(50) NOT NULL,
+    `dni` VARCHAR(8) NOT NULL,
+    `rol` VARCHAR(191) NOT NULL DEFAULT 'admin',
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `administradores_email_key`(`email`),
+    UNIQUE INDEX `administradores_dni_key`(`dni`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `asesor_especialidad` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id_asesor` INTEGER NOT NULL,
+    `id_especialidad` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `usuario_servicio` ADD CONSTRAINT `usuario_servicio_id_usuario_fkey` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `usuario_servicio` ADD CONSTRAINT `usuario_servicio_id_servicio_fkey` FOREIGN KEY (`id_servicio`) REFERENCES `servicios`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `asignaciones` ADD CONSTRAINT `asignaciones_id_asesor_fkey` FOREIGN KEY (`id_asesor`) REFERENCES `asesores`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `asignaciones` ADD CONSTRAINT `asignaciones_id_usuarios_fkey` FOREIGN KEY (`id_usuarios`) REFERENCES `usuarios`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `asesor_especialidad` ADD CONSTRAINT `asesor_especialidad_id_asesor_fkey` FOREIGN KEY (`id_asesor`) REFERENCES `asesores`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `asesor_especialidad` ADD CONSTRAINT `asesor_especialidad_id_especialidad_fkey` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
